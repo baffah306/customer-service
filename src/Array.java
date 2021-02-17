c27f8849c4a1dcf9489adedc4966e97649bc93e1
akinmade <luq_mi3@yahoo.com>
2021-02-17 16:27:46 +0100

    corrected the error in the Array

--- src/Array.java
+++ src/Array.java
@@ -11,13 +11,33 @@ public class Array {
         Scanner car_type = new Scanner(System.in);
         int car = car_type.nextInt();
         switch (car) {
-            case 1 -> System.out.println(cars[0] + " is from " + country[0]);
+            case 1:
+                System.out.println(cars[0]+" is from "+country[0]);
+                break;
+            case 2:
+                System.out.println(cars[1]+" is from "+country[1]);
+                break;
+            case 3:
+                System.out.println(cars[2]+" is from "+country[2]);
+                break;
+            case 4:
+                System.out.println(cars[3]+" is from "+country[3]);
+                break;
+            case 5:
+                System.out.println(cars[4]+" is from "+country[4]);
+                break;
+            case 6:
+                System.out.println(cars[5]+" is from "+country[5]);
+                break;
+            default:
+                System.out.println("Invalid Selection!");
+            /*case 1 -> System.out.println(cars[0] + " is from " + country[0]);
             case 2 -> System.out.println(cars[1] + " is from " + country[1]);
             case 3 -> System.out.println(cars[2] + " is from " + country[2]);
             case 4 -> System.out.println(cars[3] + " is from " + country[3]);
             case 5 -> System.out.println(cars[4] + " is from " + country[4]);
             case 6 -> System.out.println(cars[5] + " is from " + country[5]);
-            default -> System.out.println("Invalid Selection!");
+            default -> System.out.println("Invalid Selection!");*/
         }
     }
 }
