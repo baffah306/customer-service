e9e6b38f6a2c2eeb22a9c2789febfa38f5ef9429
akinmade <luq_mi3@yahoo.com>
2021-02-17 16:03:13 +0100

    Modified the code to use advanced switch

--- src/Array.java
+++ src/Array.java
@@ -1,11 +1,23 @@
+package gov.ng.gifmis;
+
+import java.util.Scanner;
+
 public class Array {
-
     public static void main(String[] args) {
-        String[] cars = {"BMW","Honda","Mazda","Toyota","Benz","Mercedes"};
-        System.out.println("My favourite car is " + cars[0]);
-        for (int index = 0; index < cars.length; index++){
-            System.out.println("Our new car is "+ cars[index]);
+        String[] cars = {"BMW","HONDA","MAZDA","TOYOTA","MERCEDES","VOLKSWAGEN","NISSAN"};
+        String[] country = {"GERMANY","JAPAN","JAPAN","JAPAN","GERMANY","GERMANY","JAPAN"};
+        for (int car_num = 0; car_num <= 6; car_num++) System.out.println((car_num+1)+"."+cars[car_num]);
+        System.out.println("Enter the number for the brand of your car: ");
+        Scanner car_type = new Scanner(System.in);
+        int car = car_type.nextInt();
+        switch (car) {
+            case 1 -> System.out.println(cars[0] + " is from " + country[0]);
+            case 2 -> System.out.println(cars[1] + " is from " + country[1]);
+            case 3 -> System.out.println(cars[2] + " is from " + country[2]);
+            case 4 -> System.out.println(cars[3] + " is from " + country[3]);
+            case 5 -> System.out.println(cars[4] + " is from " + country[4]);
+            case 6 -> System.out.println(cars[5] + " is from " + country[5]);
+            default -> System.out.println("Invalid Selection!");
         }
-
     }
 }
